be93399480e2486a61e00440bba645c34596bbf6
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-03-29 21:28:34 -0700

    fixed the dashboard

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,22 +4,24 @@ const Workout = require("../models/Workout.js");
 
 module.exports = app => {
   app.post("/api/workouts", ({ body }, res) => {
-    workout
-      .create(body)
+    Workout.create(body)
       .then(dbExercise => res.json(dbExercise))
       .catch(err => res.status(400).json(err));
   });
 
   app.put(`/api/workouts/:id`, ({ body, params }, res) => {
     console.log(body);
-    workout
-      .findOneAndUpdate({ _id: params.id }, { $push: { exercises: body } })
-      .then(dbExercise => {
-        console.log(err);
-      });
+    Workout.findOneAndUpdate({ _id: params.id }, { $push: { exercises: body } })
+      .then(dbExercise => res.json(dbExercise))
+      .catch(err => res.status(400).json(err));
   });
 
   app.get(`/api/workouts`, (req, res) => {
+    Workout.find({})
+      .then(dbExercise => res.json(dbExercise))
+      .catch(err => res.status(400).json(err));
+  });
+  app.get(`/api/workouts/range`, (req, res) => {
     Workout.find({})
       .then(dbExercise => res.json(dbExercise))
       .catch(err => res.status(400).json(err));
